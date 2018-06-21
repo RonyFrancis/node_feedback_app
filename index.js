b51c070ac692e43dbcfecba6db7da30db74626f3
antony rony <antony.rony@navigantbpm.com>
2018-06-21 07:38:05 +0530

    middleware added

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const app = express();
 
+// execute before url method is called
+app.use((req, res, next) => {
+  console.log('before action');
+  next();
+});
+
 // root url
 app.get('/', (req, res, next) => {
     res.send({
       msg: 'hello world',
     });
+    next();
   }
 );
+
+// execute before url method is called
+app.use((req, res, next) => {
+  console.log('after action');
+});
 
 // Dynamic port binding
 const PORT = process.env.PORT || 5000;
