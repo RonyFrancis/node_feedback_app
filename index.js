23d331b52297835f9bf8d1410e16718a35f91c8a
antony rony <antony.rony@navigantbpm.com>
2018-06-21 07:25:34 +0530

    changed url to '/'

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/hello', (req, res, next) => {
+app.get('/', (req, res, next) => {
     res.send({
       msg: 'hello world!',
     });
